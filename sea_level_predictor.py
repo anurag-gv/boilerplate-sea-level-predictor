9f4069b6c67f1b372056827d0f9a66927d7a331e
Anurag <107977228+anurag-gv@users.noreply.github.com>
2025-07-16 21:37:49 +0530

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,12 +12,13 @@ def draw_plot():
     x_2050 = pd.DataFrame(np.arange(2013,2051), columns=['Year'])
     x_for = pd.concat([x,x_2050]).reset_index().drop(columns='index')
     y = df.iloc[:,0]
-    plt.scatter(x, y)
+    fig, ax = plt.subplots()
+    ax.scatter(x, y)    
 
     # Create first line of best fit
     regr1 = linregress(x, y)
     m1, c1 = (regr1.slope, regr1.intercept)
-    plt.plot(x_for, m1*x_for+c1, color='yellow', label='line1')
+    ax.plot(x_for, m1*x_for+c1, color='yellow', label='line1')
 
     # Create second line of best fit
     x_rec = (x[-14:].reset_index(drop=True))#['Year']
@@ -25,13 +26,12 @@ def draw_plot():
     regr2 = linregress(x_rec, y_rec)
     m2, c2 = (regr2.slope, regr2.intercept)
     x_for2 = pd.concat([x_rec,x_2050]).reset_index().drop(columns='index')['Year']
-    plt.plot(x_for2, m2*x_for2+c2, color='green', label='line2')
+    ax.plot(x_for2, m2*x_for2+c2, color='green', label='line2')
 
     # Add labels and title
-    plt.xlabel("Year")
-    plt.ylabel("Sea Level (inches)")
-    plt.title("Rise in Sea Level")
-    plt.show()
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Sea Level (inches)")
+    ax.set_title("Rise in Sea Level")
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
